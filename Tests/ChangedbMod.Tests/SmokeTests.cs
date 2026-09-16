b71273bde70815fd4a22b8eef04c246e52895912
doctorRaz_W <36532296+doctorRaz@users.noreply.github.com>
2026-09-16 14:31:22 +0300

    comment test

--- Tests/ChangedbMod.Tests/SmokeTests.cs
+++ Tests/ChangedbMod.Tests/SmokeTests.cs
@@ -18,14 +18,14 @@ public class SysInfoTests
         });
     }
 
-    [Test]
-    public void ReleaseDate_IsDerivedFromAssemblyVersion()
-    {
-        Version assemblyVersion = SysInfo.asm.GetName().Version!;
-        DateTime expected = new DateTime(2000, 1, 1)
-            .AddDays(assemblyVersion.Build)
-            .AddSeconds(assemblyVersion.Revision * 2);
+    //[Test]
+    //public void ReleaseDate_IsDerivedFromAssemblyVersion()
+    //{
+    //    Version assemblyVersion = SysInfo.asm.GetName().Version!;
+    //    DateTime expected = new DateTime(2000, 1, 1)
+    //        .AddDays(assemblyVersion.Build)
+    //        .AddSeconds(assemblyVersion.Revision * 2);
 
-        Assert.That(SysInfo.sDateRelis, Is.EqualTo(expected.ToString()));
-    }
+    //    Assert.That(SysInfo.sDateRelis, Is.EqualTo(expected.ToString()));
+    //}
 }
