8993b48a5f0c994952cf08b08485a8e4705fd80e
oiltest90-dev <oiltest90@gmail.com>
2026-09-26 16:30:18 +0300

    refactor: isolate Multicad reflection

--- src/CadCommands/CadCommand.cs
+++ src/CadCommands/CadCommand.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Diagnostics;
-using System.Reflection;
 
 #if NC
 
@@ -16,94 +14,50 @@ using Ed = Autodesk.AutoCAD.EditorInput;
 using Rtm = Autodesk.AutoCAD.Runtime;
 
 #endif
+
 [assembly: Rtm.CommandClass(typeof(drz.ChangeDBmod.CadCommand))]
 
 namespace drz.ChangeDBmod
 {
     /// <summary>
-    /// Команды
+    /// Команды ChangeDBmod.
     /// </summary>
     internal class CadCommand : Rtm.IExtensionApplication
     {
-        private static readonly MethodInfo _multicadParam = FindMulticadParam();
-
+        /// <summary>
+        /// Инициализирует расширение.
+        /// </summary>
         public void Initialize()
         {
         }
 
+        /// <summary>
+        /// Завершает работу расширения.
+        /// </summary>
         public void Terminate()
         {
-            // throw new System.NotImplementedException();
         }
 
         /// <summary>
-        /// Переключатель баз MultiCad
+        /// Переключает базу данных Multicad.
         /// </summary>
         [Rtm.CommandMethod("drz_changedb", Rtm.CommandFlags.Session)]
         public void ChangedbMod()
         {
             App.Document doc = App.Application.DocumentManager.MdiActiveDocument;
-
             Ed.Editor ed = doc.Editor;
 
             Ed.PromptStringOptions opts = new Ed.PromptStringOptions("enter base:")
             {
                 AllowSpaces = true
             };
+
             Ed.PromptResult pr = ed.GetString(opts);
 
             if (Ed.PromptStatus.OK == pr.Status)
             {
-                if (_multicadParam == null)
-                {
-                    throw new InvalidOperationException("McParamManager.SetParam не найден");
-                }
-                object value = pr.StringResult;
-                _multicadParam.Invoke(null, new object[] { value, 9 });
+                MulticadReflection.SetParam(pr.StringResult, 9);
             }
-        }
-
-        private static MethodInfo FindMulticadParam()
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                try
-                {
-                    Type type =
-                        assembly.GetType("Multicad.ApplicationServices.McParamManager", false)
-                        ?? assembly.GetType("Multicad.AplicationServices.McParamManager", false);
-
-                    if (type == null)
-                    {
-                        continue;
-                    }
-
-                    MethodInfo method = type.GetMethod(
-                        "SetParam",
-                        new[] { typeof(object).MakeByRefType(), typeof(int) });
-
-                    if (method != null)
-                    {
-                        sw.Stop();
-                        Debug.WriteLine(
-                            $"McParamManager.SetParam найден за {sw.ElapsedMilliseconds} мс"
-                        );
-                        return method;
-                    }
-                }
-                catch
-                {
-                    // Пропускаем нативные и смешанные сборки.
-                }
-            }
-
-            sw.Stop();
-            Debug.WriteLine(
-                $"McParamManager.SetParam не найден, поиск занял {sw.ElapsedMilliseconds} мс");
-
-            return null;
         }
     }
 }
